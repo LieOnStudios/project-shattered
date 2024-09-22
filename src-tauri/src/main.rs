7dd08e8cc5b976e3ad5d85e54849c7e79115f515
lieonlion <lieonlion4@gmail.com>
2024-09-22 16:38:14 +0100

    innit commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,49 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::fs::{ self };
+
+#[tauri::command]
+fn save_data_to_file(data_str: &str, path: &str) {
+    if fs::metadata(path).is_err() {
+        let split_path = path.split("/");
+        let no_file_path = path.replace(split_path.last().unwrap(), "");
+        let _ = fs::create_dir_all(no_file_path);
+    }
+    let _ = fs::write(path, data_str);
+}
+
+#[tauri::command]
+fn load_data_from_file(path: &str) -> String {
+    if fs::metadata(path).is_err() {
+        return "error 404".to_string();
+    }
+    let json = fs::read_to_string(path).unwrap();
+    json.into()
+}
+
+#[tokio::main]
+async fn load_data_from_https(url: &str, headers: &str) -> Result<serde_json::Value, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let headers_json_wraped: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(headers);
+    let header_json = headers_json_wraped.unwrap();
+    let data: serde_json::Value = client.get(url).header(header_json["key"].as_str().unwrap(), header_json["value"].as_str().unwrap()).send().await?.json().await?;
+
+    Ok(data)
+}
+
+#[tauri::command]
+fn get_data_from_https(url: &str, headers: &str) -> String {
+    let data = load_data_from_https(url, headers);
+    if data.is_err() {
+        println!("{}", data.unwrap_err());
+        return "error".to_string()
+    }
+    serde_json::to_string_pretty(&data.unwrap()).unwrap()
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![load_data_from_file, save_data_to_file, get_data_from_https])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
