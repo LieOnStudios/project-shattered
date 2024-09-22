fc978ef972496a0d41498f5c1301bfdbea198d91
lieonlion <lieonlion4@gmail.com>
2024-09-22 16:55:39 +0100

    added servor and js code from vacuum

--- src/main.js
+++ src/main.js
@@ -16,3 +16,32 @@ window.addEventListener("DOMContentLoaded", () => {
     greet();
   });
 });
+
+async function save_data() {
+  let data_str = JSON.stringify( {curseforge: greetInputEl.value}, null, "\t" );
+  invoke("save_data_to_file", { dataStr: data_str, path: "../data/ids.json" });
+  let file_data = await invoke("load_data_from_file", { path: "../data/ids.json" });
+  console.log( file_data );
+}
+
+// async function load_data() {
+//   let file_data = await invoke("load_data_from_file", { path: "../data/ids.json" });
+//   let json = JSON.parse(file_data);
+//   greetInputEl.value = json.curseforge;
+// }
+
+// load_data();
+
+// greetInputEl.addEventListener("input", () => {
+//   greetMsgEl = document.querySelector("#greet-msg");
+//   greet();
+// });
+
+async function load_data() {
+  let file_data = await invoke("get_data_from_https", { url: "https://api.modrinth.com/v2/project/HKKqmr2p", headers: `{"key": "x-api-key", "value": ""}` });
+  let header = await invoke("load_data_from_file", { path: "../data/ids.json" });
+  let header_json = JSON.parse(header);
+  // let file_data = await invoke("get_data_from_https", { url: "https://api.curseforge.com/v1/mods/858032", headers: `{"key": "x-api-key", "value": "${header_json["curseforge"]}"}` });
+  // let json = JSON.parse(file_data);
+  console.log(file_data);
+}
