6117544dc5e9fef8b3f6c22439a87ba761da39fd
lieonlion <lieonlion4@gmail.com>
2024-09-22 20:48:08 +0100

    some basic updates

--- src/main.js
+++ src/main.js
@@ -1,38 +1,16 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
-
 async function save_data() {
-  let data_str = JSON.stringify( {curseforge: greetInputEl.value}, null, "\t" );
-  invoke("save_data_to_file", { dataStr: data_str, path: "../data/ids.json" });
-  let file_data = await invoke("load_data_from_file", { path: "../data/ids.json" });
+  let data_str = JSON.stringify({ curseforge: "" }, null, "\t");
+  invoke("save_data_to_file", { dataStr: data_str, path: "../data/keys.json" });
+  let file_data = await invoke("load_data_from_file", { path: "../data/keys.json" });
   console.log( file_data );
 }
 
-// greetInputEl.addEventListener("input", () => {
-//   greetMsgEl = document.querySelector("#greet-msg");
-//   greet();
-// });
-
 async function load_data() {
   let file_data = await invoke("get_data_from_https", { url: "https://api.modrinth.com/v2/project/HKKqmr2p", headers: `{"key": "x-api-key", "value": ""}` });
-  let header = await invoke("load_data_from_file", { path: "../data/ids.json" });
-  let header_json = JSON.parse(header);
+  let curseforge_key = await invoke("load_data_from_file", { path: "../data/keys.json" });
+  let curseforge_key_json = JSON.parse(curseforge_key);
   // let file_data = await invoke("get_data_from_https", { url: "https://api.curseforge.com/v1/mods/858032", headers: `{"key": "x-api-key", "value": "${header_json["curseforge"]}"}` });
   // let json = JSON.parse(file_data);
   console.log(file_data);
