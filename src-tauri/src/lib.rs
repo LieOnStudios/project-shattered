50d8458c76ad56dfed4b193ddde00130a0fa8e46
lieonlion <lieonlion4@gmail.com>
2024-10-10 11:10:10 +0100

    comments and cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,12 +15,16 @@ fn save_data_to_file(data_str: &str, path: &str) {
 
 #[tauri::command]
 fn load_data_from_file(path: &str) -> String {
+    // Get file's absolute path.
+    // File err check (if it exists and can be read).
+    let absolute_path = path::absolute(path);
     let err_check = fs::metadata(path);
     if err_check.is_err() {
-        return err_check.unwrap_err().to_string();
+        // Returns err and file path.
+        return err_check.unwrap_err().to_string() + " from file " + &absolute_path.unwrap().display().to_string();
     }
-    let file_path = path::absolute(path);
-    println!("{}", file_path.unwrap().display().to_string());
+    // Read file data.
+    // Returns file data.
     let file_data = fs::read_to_string(path).unwrap();
     file_data.into()
 }
@@ -28,42 +32,47 @@ fn load_data_from_file(path: &str) -> String {
 // Fixed by @NotGhoull - https://github.com/LieOnLion/project-shattered/commit/3dd2e1ec6600795a99533b8e43b643b916078f56
 fn convert_json_to_headers(data: &str) -> HeaderMap {
     let mut map = HeaderMap::new();
-
-    // Deserialize the JSON string into a serde_json::Value
+    // Deserialize the JSON string into a serde_json::Value.
     let data_json = serde_json::from_str::<serde_json::Value>(data).unwrap();
 
-    // Check if the root element is an array of headers
+    // Check if the root element is an array of headers.
     if let Some(headers) = data_json.as_array() {
         for header in headers {
-            // Clone header name and value to ensure they have an owned String type
-            let header_key = header["header"].as_str().unwrap().to_string();
+            // Clone header name and value to ensure they have an owned String type.
+            let header_name = header["header"].as_str().unwrap().to_string();
             let header_value = header["value"].as_str().unwrap().to_string();
 
-            // Insert the owned values into the HeaderMap
-            map.insert(header_key.parse::<HeaderName>().unwrap(), HeaderValue::from_str(&header_value).unwrap());
+            // Insert the owned values into the HeaderMap.
+            map.insert(header_name.parse::<HeaderName>().unwrap(), HeaderValue::from_str(&header_value).unwrap());
         }
     }
-
-    // Return HeaderMap
+    // Returns HeaderMap.
     map
 }
 
 #[tokio::main]
 async fn load_data_from_https(url: &str, headers: &str) -> Result<serde_json::Value, reqwest::Error> {
+    // Create a new Client.
+    // Send a GET request to server with headers.
     let client = reqwest::Client::new();
     let data: serde_json::Value = client
         .get(url).headers(convert_json_to_headers(&headers))
         .send().await?.json().await?;
 
+    // Returns data and err.
     Ok(data)
 }
 
 #[tauri::command]
 fn get_data_from_https(url: &str, headers: &str) -> String {
+    // Passes values into load_data_from_https().
+    // Data err check.
     let data = load_data_from_https(url, headers);
     if data.is_err() {
+        // Returns data err.
         return data.unwrap_err().to_string();
     }
+    // Returns the data in JSON (pretty).
     serde_json::to_string_pretty(&data.unwrap()).unwrap()
 }
 
