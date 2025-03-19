ea740a5f95e90eec7613d160e6891c6dc196ef5b
LieOnLion <lieonlion4@gmail.com>
2025-03-19 22:03:20 +0000

    buttons pt. 2

--- src/index.tsx
+++ src/index.tsx
@@ -9,21 +9,24 @@ import TitleBar from "./components/title_bar";
 import NavBar from "./components/nav_bar";
 
 interface Props {
-	children?: HTMLElement;
+  children?: HTMLElement;
 }
 
 const App: Component<Props> = (props) => {
-	return (
-		<>
-			<NavBar />
-			<div class="flex flex-col grow overflow-hidden">
-				<TitleBar />
-				<main class="flex flex-col grow overflow-auto gap-72 mr-2 mb-2 p-2 rounded-lg bg-slate-200 dark:bg-slate-800">
-					{props.children}
-				</main>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <NavBar />
+      <div class="flex flex-col grow overflow-hidden">
+        <TitleBar />
+        <main class="flex flex-col grow overflow-auto gap-4 mr-2 mb-2 p-2 rounded-lg bg-slate-200 dark:bg-slate-800">
+          {props.children}
+        </main>
+      </div>
+    </>
+  );
 };
 
-render(() => <Routes root={App} />, document.getElementById("root") as HTMLElement);
+render(
+  () => <Routes root={App} />,
+  document.getElementById("root") as HTMLElement
+);
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,46 @@
 import { Component } from "solid-js";
 import { Button } from "../components/ui/button";
-import { Maximize } from "lucide-solid";
+import { HomeIcon, Link, Maximize, Trash, Upload } from "lucide-solid";
 
 const Home: Component = () => {
-	return (
-		<>
-			<div class="inline-flex gap-4 items-center">
-				<Button variant="ghost">
-					<span>Primary</span>
-				</Button>
-				<Button variant="destructive">
-					<span>Primary</span>
-				</Button>
-				<Button variant="secondary">Secondary</Button>
-				<Button variant="destructive">Destructive</Button>
-				<Button variant="outline">Outline</Button>
-				<Button size="icon">
-					<Maximize size={16} />
-				</Button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div class="inline-flex gap-4 items-center">
+        <Button>
+          <span>Primary</span>
+        </Button>
+        <Button variant="secondary">
+          <span>Secondary</span>
+        </Button>
+        <Button variant="destructive">
+          <span>Destructive</span>
+        </Button>
+        <Button variant="outline">
+          <span>Outline</span>
+        </Button>
+        <Button variant="ghost">
+          <span>Ghost</span>
+        </Button>
+      </div>
+      <div class="inline-flex gap-4 items-center">
+        <Button size="icon">
+          <HomeIcon size={16} />
+        </Button>
+        <Button variant="secondary" size="icon">
+          <Link size={16} />
+        </Button>
+        <Button variant="destructive" size="icon">
+          <Trash size={16} />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Upload size={16} />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Maximize size={16} />
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
